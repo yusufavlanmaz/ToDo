6d862291a652ca63c17224af97e435acbb6ad489
yusufavlanmaz <yusufavz@hotmail.com>
2022-01-23 22:18:05 +0300

    tobe contiuned

---
+++ src/components/ıtemBox/ItemBox.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {View,Text, TextInput,} from 'react-native';
+const ItemBox = () => {
+    return(
+        <View>
+            <TextInput 
+            style={styles.input}
+            placeholder="Yapılacak..."
+            placeholder='eaeaea'
+            
+            
+            />
+        </View>
+    )
+}
+export default ItemBox;
